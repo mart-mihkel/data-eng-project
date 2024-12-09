22aa97e4dbb457e1f77df0061301205f2b899492
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-12-09 16:09:18 +0200

    time dim in fact pseudocode

--- dbt/models/star/accident_fact.sql
+++ dbt/models/star/accident_fact.sql
@@ -1,13 +1,22 @@
+WITH weather_dim AS {{ ref('weather_dim') }}
+WITH location_dim AS {{ ref('location_dim') }}
+WITH parties_dim AS {{ ref('parties_dim') }}
+WITH road_dim AS {{ ref('road_dim') }}
+WITH time_dim AS {{ ref('time_dim') }}
+
 SELECT
-        case_id AS id,
-        case_id AS time_id,
+        a.case_id AS id,
+        t.id AS time_id,
         case_id AS location_id,
         case_id AS weather_id,
         case_id AS parties_id,
         case_id AS road_id,
-        time AS happened_at,
-        num_people AS number_of_people,
-        num_vehicles AS number_of_vehicles,
-        num_dead AS uumber_of_fatalities,
-        num_injured AS number_of_injured
-FROM accidents_tmp
+        a.num_people, 
+        a.num_vehicles,
+        a.num_dead,
+        a.num_injured
+FROM accidents_tmp AS a
+JOIN time_dim AS t
+        ON t.year = a.year
+        AND y.month = a.month
+        AND y.day = a.day

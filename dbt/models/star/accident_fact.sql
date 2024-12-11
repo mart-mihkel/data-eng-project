2e57921afa7355379995e925e4f5ec547605de96
Julius Välja <kasutaja@JVL-11>
2024-12-11 15:35:24 +0200

    Simplified sql for now

--- dbt/models/star/accident_fact.sql
+++ dbt/models/star/accident_fact.sql
@@ -1,12 +1,6 @@
-with weather_dim as {{ ref('weather_dim') }}
-with location_dim as {{ ref('location_dim') }}
-with parties_dim as {{ ref('parties_dim') }}
-with road_dim as {{ ref('road_dim') }}
-with time_dim as {{ ref('time_dim') }}
-
 select
         a.case_id as id,
-        t.id as time_id,
+        case_id as time_id,
         case_id as location_id,
         case_id as weather_id,
         case_id as parties_id,
@@ -15,8 +9,4 @@ select
         a.num_vehicles,
         a.num_dead,
         a.num_injured
-from accidents_tmp as a
-join time_dim as t
-        using t.year = a.year
-        and y.month = a.month
-        and y.day = a.day
+from accidents_tmp a

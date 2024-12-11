e908725c060cff1463f6e8c349383f426f398514
mart-mihkel <mart.mihkel.aun@gmail.com>
2024-12-11 16:49:32 +0200

    redis

--- streamlit/app/querier.py
+++ streamlit/app/querier.py
@@ -1,20 +1,26 @@
 import redis
 import duckdb
+import pickle
 import pandas as pd
 
 
-class Querier():
-    def __init__(self):
-        self.duckdb = duckdb.connect('../duckdb/duck.db')
+class CachedQuery():
+    def __init__(self, *, duckdb_readonly=True):
+        self.duckdb = duckdb.connect('../duckdb/duck.db', read_only=duckdb_readonly)
         self.redis = redis.Redis(host='redis', port=6379, db=0)
 
 
     def query_cached(self, *, sql: str, cache_key: str) -> pd.DataFrame:
-        res = self.redis.get(cache_key)
+        cache_res = self.redis.get(cache_key)
 
-        if res is None:
+        if cache_res is None:
             res = self.duckdb.sql(sql).fetchall()
+            df = pd.DataFrame(res)
 
-        return pd.DataFrame(res)
+            self.redis.set(cache_key, pickle.dumps(df))
+        else:
+            df = pickle.loads(cache_res)
+
+        return df
 
 
